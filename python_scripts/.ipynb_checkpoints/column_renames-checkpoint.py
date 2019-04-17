13596f244d40f6e6096a0a513300260f6cce6f0a
Thomas Hirsch <thomas.hirsch@justice.gov.uk>
2019-04-17 15:38:33 +0000

    initial commit

---
+++ python_scripts/.ipynb_checkpoints/column_renames-checkpoint.py
@@ -0,0 +1,19 @@
+import json
+bookings_renames = {'id': 'id',
+                    'timeFrom': 'time_from',
+                    'timeTo': 'time_to',
+                    'audit.created.when': 'created',
+                    'locationId': 'location_id',
+                    'status': 'status',
+                    'statusReason': 'status_reason'}
+
+with open("metadata/bookings_renames.json", "w") as fp:
+    json.dump(bookings_renames, fp)
+    
+location_renames = {'id': 'id',
+                    'name': 'name',
+                    'longQualifier': 'long_qualifier',
+                    'capacity': 'capacity'}
+
+with open("metadata/locations_renames.json", "w") as fp:
+    json.dump(location_renames, fp)
