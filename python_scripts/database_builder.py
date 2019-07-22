89bea100db3476654b69a813a187b18b62b8623a
Thomas Hirsch <thomas.hirsch@justice.gov.uk>
2019-07-22 13:34:39 +0000

    lint and add revised metadata conformance

--- python_scripts/database_builder.py
+++ python_scripts/database_builder.py
@@ -1,31 +1,49 @@
 from etl_manager.meta import DatabaseMeta, TableMeta
 
-def build_database():
-    db = DatabaseMeta(name = 'matrix_db', bucket='alpha-dag-matrix')
 
-    # Create table meta object
-    bookings = TableMeta(name = 'bookings', location = 'bookings')
+db = DatabaseMeta(name="matrix_db", bucket="alpha-dag-matrix")
 
-    # Add column defintions to the table
-    bookings.add_column(name = 'id', type = 'character', description = 'Booking id')
-    bookings.add_column(name = 'time_from',type = 'datetime', description = 'Start time of booking')
-    bookings.add_column(name = 'time_to',type = 'datetime', description = 'End time of booking')
-    bookings.add_column(name = 'created',type = 'datetime', description = 'Time the booking was created')
-    bookings.add_column(name = 'location_id', type ='character', description = 'id to match to location')
-    bookings.add_column(name = 'status',type = 'character', description = 'One of APPROVED, TENTATIVE or CANCELLED')
-    bookings.add_column(name = 'status_reason', type ='character', description = 'Reason for cancellation where relevant')
-    db.add_table(bookings)
+# Create table meta object
+bookings = TableMeta(name="bookings", location="bookings")
 
-    locations = TableMeta(name = 'locations', location = 'locations', data_format = 'csv_quoted_nodate')
+# Add column defintions to the table
+bookings.add_column(name="id", type="character", description="Booking id")
+bookings.add_column(
+    name="time_from", type="datetime", description="Start time of booking"
+)
+bookings.add_column(name="time_to", type="datetime", description="End time of booking")
+bookings.add_column(
+    name="created", type="datetime", description="Time the booking was created"
+)
+bookings.add_column(
+    name="location_id", type="character", description="id to match to location"
+)
+bookings.add_column(
+    name="status",
+    type="character",
+    description="One of APPROVED, TENTATIVE or CANCELLED",
+)
+bookings.add_column(
+    name="status_reason",
+    type="character",
+    description="Reason for cancellation where relevant",
+)
+db.add_table(bookings)
 
-    locations.add_column(name = 'id', type = 'character', description = 'locationId')
-    locations.add_column(name = 'name', type = 'character', description = 'location name')
-    locations.add_column(name = 'long_qualifier', type = 'character', description = 'long qualifier for location')
-    locations.add_column(name = 'capacity', type = 'character', description = 'room capacity')
-    db.add_table(locations)
+locations = TableMeta(
+    name="locations", location="locations", data_format="csv_quoted_nodate"
+)
+
+locations.add_column(name="id", type="character", description="locationId")
+locations.add_column(name="name", type="character", description="location name")
+locations.add_column(
+    name="long_qualifier", type="character", description="long qualifier for location"
+)
+locations.add_column(name="capacity", type="character", description="room capacity")
+db.add_table(locations)
 
 
-    bookings.write_to_json('metadata/bookings.json')
-    locations.write_to_json('metadata/locations.json')
+bookings.write_to_json("metadata/bookings.json")
+locations.write_to_json("metadata/locations.json")
 
-    db.create_glue_database(delete_if_exists = True)
+db.create_glue_database(delete_if_exists=True)
