940c6175acd36214bdbec75c9f54338865593bd0
Thomas Hirsch <thomas.hirsch@justice.gov.uk>
2019-08-27 11:01:34 +0000

    add cancelled time and person id

--- python_scripts/column_renames.py
+++ python_scripts/column_renames.py
@@ -3,13 +3,14 @@ bookings_renames = {'id': 'id',
                     'timeFrom': 'time_from',
                     'timeTo': 'time_to',
                     'audit.created.created': 'created',
+                    'audit.cancelled.created': 'cancelled_datetime',
                     'locationId': 'location_id',
                     'status': 'status',
                     'statusReason': 'status_reason'}
 
 with open("metadata/bookings_renames.json", "w") as fp:
     json.dump(bookings_renames, fp)
-    
+
 location_renames = {'id': 'id',
                     'name': 'name',
                     'longQualifier': 'long_qualifier',
--- python_scripts/database_builder.py
+++ python_scripts/database_builder.py
@@ -7,39 +7,25 @@ db = DatabaseMeta(name="matrix_db", bucket="alpha-dag-matrix")
 bookings = TableMeta(name="bookings", location="bookings", data_format="parquet")
 
 # Add column defintions to the table
-bookings.add_column(name="id", type="int", description="Booking id")
-bookings.add_column(
-    name="time_from", type="datetime", description="Start time of booking"
-)
+bookings.add_column(name="id", type="character", description="Booking id")
+bookings.add_column(name="time_from", type="datetime", description="Start time of booking")
 bookings.add_column(name="time_to", type="datetime", description="End time of booking")
-bookings.add_column(
-    name="created", type="datetime", description="Time the booking was created"
-)
-bookings.add_column(
-    name="location_id", type="int", description="id to match to location"
-)
-bookings.add_column(
-    name="status",
-    type="character",
-    description="One of APPROVED, TENTATIVE or CANCELLED",
-)
-bookings.add_column(
-    name="status_reason",
-    type="character",
-    description="Reason for cancellation where relevant",
-)
+bookings.add_column(name="created", type="datetime", description="Time the booking was created")
+bookings.add_column(name="cancelled_time", type="datetime", description="Time of cancellation")
+bookings.add_column(name="location_id", type="character", description="id to match to location")
+bookings.add_column(name="owner_id", type="character", description="id of user who owns the booking")
+bookings.add_column(name="booked_by_id", type="character", description="id of user who created the booking")
+bookings.add_column(name="status", type="character", description="One of APPROVED, TENTATIVE or CANCELLED")
+bookings.add_column(name="status_reason",type="character", description="Reason for cancellation where relevant")
+
 db.add_table(bookings)
 
-locations = TableMeta(
-    name="locations", location="locations", data_format="parquet"
-)
+locations = TableMeta(name="locations", location="locations", data_format="parquet")
 
-locations.add_column(name="id", type="int", description="locationId")
+locations.add_column(name="id", type="character", description="locationId")
 locations.add_column(name="name", type="character", description="location name")
-locations.add_column(
-    name="long_qualifier", type="character", description="long qualifier for location"
-)
-locations.add_column(name="capacity", type="double", description="room capacity")
+locations.add_column(name="long_qualifier", type="character", description="long qualifier for location")
+locations.add_column(name="capacity", type="character", description="room capacity")
 db.add_table(locations)
 
 
