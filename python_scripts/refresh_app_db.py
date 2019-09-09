d0d052c7750956d5997e333e9e0c81efebcd0acd
Thomas Hirsch <thomas.hirsch@justice.gov.uk>
2019-09-09 15:28:41 +0000

    add refresh app db

---
+++ python_scripts/refresh_app_db.py
@@ -0,0 +1,41 @@
+import pydbtools as pydb
+
+def refresh_app_db():
+    pydb.get_athena_query_response("drop database if exists matrixbooking_app_db cascade")
+    delete_all_matching_s3_objects("alpha-app-matrixbooking", "db")
+
+    pydb.get_athena_query_response("create database matrixbooking_app_db location 's3://alpha-app-matrixbooking/db/'")
+
+    pydb.get_athena_query_response(
+        """
+        create table if not exists matrixbooking_app_db.bookings
+        with(external_location = 's3://alpha-app-matrixbooking/db/bookings/')
+        as select b.*, l.name, l.long_qualifier, l.capacity from matrix_db.bookings as b
+                        inner join matrix_db.locations as l
+                        on b.location_id = l.id
+                        inner join occupeye_db_live.sensors as s
+                        on l.id = s.location
+        """)
+
+
+    pydb.get_athena_query_response(
+        """
+        create table if not exists matrixbooking_app_db.locations
+        with(external_location = 's3://alpha-app-matrixbooking/db/locations/')
+        as select * from matrix_db.locations as l
+                                   inner join occupeye_db_live.sensors as s
+                                   on l.id = s.location
+        """)
+
+    pydb.get_athena_query_response(
+        """
+        create table if not exists matrixbooking_app_db.sensor_observations
+        with(external_location = 's3://alpha-app-matrixbooking/db/sensor_observations')
+        as
+        select so.obs_datetime, so.sensor_value, se.*
+        from occupeye_db_live.sensor_observations as so
+        inner join occupeye_db_live.sensors as se
+        on so.survey_device_id = se.surveydeviceid
+        inner join matrix_db.locations as l
+        on l.id = se.location
+        """)
--- python_scripts/s3_utils.py
+++ python_scripts/s3_utils.py
@@ -2,6 +2,10 @@ import boto3
 import botocore
 import json
 
+
+s3_resource = boto3.resource('s3')
+s3 = boto3.client('s3')
+
 def read_json_from_s3(s3_path):
     bucket, key = s3_path_to_bucket_key(s3_path)
     obj = s3_resource.Object(bucket, key)
@@ -20,5 +24,41 @@ def s3_object_exists(bucket, path):
     except botocore.exceptions.ClientError as e:
         return False
 
-s3_resource = boto3.resource('s3')
 
+def get_matching_s3_keys(bucket, prefix='', suffix=''):
+
+    """
+    Generate the keys in an S3 bucket.
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetch keys that start with this prefix (optional).
+    :param suffix: Only fetch keys that end with this suffix (optional).
+    """
+    s3 = boto3.client('s3')
+    kwargs = {'Bucket': bucket}
+
+    # If the prefix is a single string (not a tuple of strings), we can
+    # do the filtering directly in the S3 API.
+    if isinstance(prefix, str):
+        kwargs['Prefix'] = prefix
+
+    while True:
+
+        # The S3 API response is a large blob of metadata.
+        # 'Contents' contains information about the listed objects.
+        resp = s3.list_objects_v2(**kwargs)
+        for obj in resp['Contents']:
+            key = obj['Key']
+            if key.startswith(prefix) and key.endswith(suffix):
+                yield key
+
+        # The S3 API is paginated, returning up to 1000 keys at a time.
+        # Pass the continuation token into the next response, until we
+        # reach the final page (when this field is missing).
+        try:
+            kwargs['ContinuationToken'] = resp['NextContinuationToken']
+        except KeyError:
+            break
+
+def delete_all_matching_s3_objects(bucket, prefix='', suffix = ''):
+    for key in get_matching_s3_keys(bucket, prefix, suffix):
+    s3.Object(bucket, key).delete()
