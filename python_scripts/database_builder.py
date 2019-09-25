9916525940f639780c57b9a71c197f0490daa0ae
Thomas Hirsch <thomas.hirsch@justice.gov.uk>
2019-09-25 10:36:54 +0000

    add joined rooms

--- python_scripts/database_builder.py
+++ python_scripts/database_builder.py
@@ -4,18 +4,14 @@ from etl_manager.meta import DatabaseMeta, TableMeta
 db = DatabaseMeta(name="matrix_db", bucket="alpha-dag-matrix")
 
 # Create table meta object
-bookings = TableMeta(
-    name="bookings", location="bookings", data_format="parquet"
-)
+bookings = TableMeta(name="bookings", location="bookings", data_format="parquet")
 
 # Add column defintions to the table
 bookings.add_column(name="id", type="character", description="Booking id")
 bookings.add_column(
     name="time_from", type="datetime", description="Start time of booking"
 )
-bookings.add_column(
-    name="time_to", type="datetime", description="End time of booking"
-)
+bookings.add_column(name="time_to", type="datetime", description="End time of booking")
 bookings.add_column(
     name="created", type="datetime", description="Time the booking was created"
 )
@@ -26,9 +22,7 @@ bookings.add_column(
     name="location_id", type="character", description="id to match to location"
 )
 bookings.add_column(
-    name="owner_id",
-    type="character",
-    description="id of user who owns the booking",
+    name="owner_id", type="character", description="id of user who owns the booking"
 )
 bookings.add_column(
     name="booked_by_id",
@@ -46,33 +40,34 @@ bookings.add_column(
     description="Reason for cancellation where relevant",
 )
 bookings.add_column(
-    name="attendee_count",
-    type="int",
-    description="Self-reported number of attendees",
+    name="attendee_count", type="int", description="Self-reported number of attendees"
 )
 
 db.add_table(bookings)
 
-locations = TableMeta(
-    name="locations", location="locations", data_format="parquet"
-)
+locations = TableMeta(name="locations", location="locations", data_format="parquet")
 
 locations.add_column(name="id", type="character", description="locationId")
+locations.add_column(name="name", type="character", description="location name")
 locations.add_column(
-    name="name", type="character", description="location name"
+    name="long_qualifier", type="character", description="long qualifier for location"
 )
-locations.add_column(
-    name="long_qualifier",
-    type="character",
-    description="long qualifier for location",
-)
-locations.add_column(
-    name="capacity", type="character", description="room capacity"
-)
+locations.add_column(name="capacity", type="character", description="room capacity")
 db.add_table(locations)
 
+joined_rooms = TableMeta(
+    name="joined_rooms", location="joined_rooms", data_format="csv"
+)
+joined_rooms.add_column(name="joined_id", type="character", description="joined id")
+joined_rooms.add_column(name="joined_name", type="character", description="joined name")
+joined_rooms.add_column(name="split_id", type="character", description="split id")
+joined_rooms.add_column(name="split_name", type="character", description="split name")
+joined_rooms.add_column(name="building", type="character", description="building")
+
+db.add_table(joined_rooms)
 
 bookings.write_to_json("metadata/bookings.json")
 locations.write_to_json("metadata/locations.json")
+joined_rooms.write_to_json("metadata/locations.json")
 
 db.create_glue_database(delete_if_exists=True)
--- python_scripts/refresh_app_db.py
+++ python_scripts/refresh_app_db.py
@@ -1,37 +1,49 @@
 import pydbtools as pydb
-from s3_utils import delete_all_matching_s3_objects
 
 
 def refresh_app_db():
-    print("refreshing app db")
-    print("dropping db")
     pydb.get_athena_query_response(
         "drop database if exists matrixbooking_app_db cascade"
     )
-    print("delete db files in bucket")
     delete_all_matching_s3_objects("alpha-app-matrixbooking", "db")
 
-    print("create database")
     pydb.get_athena_query_response(
-        """create database matrixbooking_app_db
-        location 's3://alpha-app-matrixbooking/db/'"""
+        "create database matrixbooking_app_db location 's3://alpha-app-matrixbooking/db/'"
     )
 
-    print("create bookings table")
     pydb.get_athena_query_response(
         """
         create table if not exists matrixbooking_app_db.bookings
         with(external_location = 's3://alpha-app-matrixbooking/db/bookings/')
-        as select distinct b.*, l.name, l.long_qualifier, l.capacity
-        from matrix_db.bookings as b
-        inner join matrix_db.locations as l
+        as
+        select distinct
+        b.*,
+        coalesce(b.joined_name, l.name) as name,
+        l.long_qualifier,
+        l.capacity
+        from (select b.id, b.time_from, b.time_to, b.created,
+            b.cancelled_time,
+            coalesce(jr.split_id, b.location_id) as location_id,
+            b.owner_id,
+            b.booked_by_id,
+            b.status,
+            b.status_reason,
+            b.attendee_count,
+            jr.split_id,
+            jr.joined_name,
+            jr.split_name
+            from
+            matrix_db.bookings as b
+            left join matrix_db.joined_rooms as jr
+            on b.location_id = jr.joined_id) as b
+        inner join
+        matrix_db.locations as l
         on b.location_id = l.id
         inner join occupeye_db_live.sensors as s
         on l.id = s.location
         """
     )
 
-    print("create locations table")
     pydb.get_athena_query_response(
         """
         create table if not exists matrixbooking_app_db.locations
@@ -42,12 +54,10 @@ def refresh_app_db():
         """
     )
 
-    print("create sensor observations table")
     pydb.get_athena_query_response(
         """
         create table if not exists matrixbooking_app_db.sensor_observations
-        with(external_location =
-        's3://alpha-app-matrixbooking/db/sensor_observations')
+        with(external_location = 's3://alpha-app-matrixbooking/db/sensor_observations')
         as
         select so.obs_datetime, so.sensor_value, se.*
         from occupeye_db_live.sensor_observations as so
@@ -57,8 +67,6 @@ def refresh_app_db():
         on l.id = se.location
         """
     )
-
-    print("create surveys table")
     pydb.get_athena_query_response(
         """
         create table if not exists matrixbooking_app_db.surveys
