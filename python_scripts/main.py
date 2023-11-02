2ee4e11810ee3cd71410d636d1619b8e255ab6bd
andrew.craik@justice.gov.uk <andrew.craik@justice.gov.uk>
2023-11-02 16:49:57 +0000

    feat: Add command line argument and update script
    Add new (optional) argument (skip write to s3)
    Allows for testing in interactive mode without affecting underlying data
    
    Retain bookings and location objects from scraper
    
    , db_version and retain bookings,location from scrape (for us

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -23,13 +23,26 @@ argp.add_argument(
     
 )
 
+# Writing to s3
+argp.add_argument(
+    "--skip-write-s3", 
+    action=argparse.BooleanOptionalAction,
+    help="If passed, this will skip writing to s3 (default is to write to s3 when scaper runs)",
+    
+)
+
+
 args = argp.parse_args()
 
 scrape_date = parse(args.scrape_date).strftime("%Y-%m-%d")
 
 if __name__=="__main__":
     
-
-    scrape_days_from_api(scrape_date, "eod", args.env)
+    # Current db_version
+    db_version = "db_v2"
+    
+    # Get bookings and locations 
+    # Optionally writes to s3 
+    bookings, locations = scrape_days_from_api(scrape_date, "eod", db_version, args.env, args.skip_write_s3)
 
     #refresh_app_db()
