f1b02925ec8948cecc14ccaea6faf18d1510284b
matt-heery <matt.heery@digital.justice.gov.uk>
2023-12-29 17:14:53 +0000

    removing reliance on dataengineeringutils

--- functions/api_requests.py
+++ functions/api_requests.py
@@ -1,19 +1,13 @@
-import re
 import pandas as pd
 import requests
 import json
 import boto3
 import gzip
 
-from dataengineeringutils.utils import read_json
-from dataengineeringutils.pd_metadata_conformance import (
-    impose_metadata_column_order_on_pd_df,
-    impose_metadata_data_types_on_pd_df,
-)
-from arrow_pd_parser import writer
+from mojap_metadata import Metadata
+from arrow_pd_parser import writer, caster
 import python_scripts.s3_utils as s3_utils
 from datetime import datetime, timedelta
-from arrow_pd_parser import writer
 from python_scripts.constants import (
     meta_path_bookings,
     table_location_bookings,
@@ -24,6 +18,11 @@ from python_scripts.constants import (
 from logging import getLogger
 
 logger = getLogger(__name__)
+
+
+def read_json(file_path):
+    f = open(file_path)
+    return json.loads(f.read())
 
 
 def get_secrets():
@@ -103,6 +102,8 @@ def unpack_row(data: dict, joiner: str = ".") -> dict:
             for key in item:
                 unpack(item[key], f"{keys_name}{key}{joiner}")
         elif isinstance(item, list):
+            if len(item) == 1:
+                unpack(item[0], f"{keys_name}{joiner}")
             for i, it in enumerate(item):
                 unpack(it, f"{keys_name}{i + 1}{joiner}")
         else:
@@ -312,7 +313,7 @@ def get_bookings_df(bookings, db_version, env, start_date, skip_write_s3=False):
     renames = read_json(f"metadata/{db_version}/{env}/bookings_renames.json")
 
     # Table metadata
-    bookings_metadata = read_json(meta_path_bookings)
+    bookings_metadata = Metadata.from_json(meta_path_bookings)
 
     if len(bookings_df) > 0:
         bookings_df = bookings_df.reindex(columns=renames.keys())
@@ -322,7 +323,7 @@ def get_bookings_df(bookings, db_version, env, start_date, skip_write_s3=False):
         bookings_df = pd.DataFrame(columns=renames.values())
 
     # Ensure column order correct
-    bookings_df = impose_exact_conformance_on_pd_df(bookings_df, bookings_metadata)
+    bookings_df = caster.cast_pandas_table_to_schema(bookings_df, bookings_metadata)
 
     # Write out dataframe, ensuring conformance with metadata
     if not skip_write_s3:
@@ -339,9 +340,11 @@ def get_locations_df(locations, db_version, env, start_date, skip_write_s3):
     locations_df = pd.json_normalize(locations)
     renames = read_json(f"metadata/{db_version}/{env}/locations_renames.json")
     locations_df = locations_df[renames.keys()].rename(columns=renames)
-    locations_metadata = read_json(f"metadata/{db_version}/{env}/locations.json")
+    locations_metadata = Metadata.from_json(
+        f"metadata/{db_version}/{env}/locations.json"
+    )
 
-    locations_df = impose_exact_conformance_on_pd_df(locations_df, locations_metadata)
+    locations_df = caster.cast_pandas_table_to_schema(locations_df, locations_metadata)
 
     if not skip_write_s3:
         writer.write(
@@ -351,70 +354,3 @@ def get_locations_df(locations, db_version, env, start_date, skip_write_s3):
         )
 
     return locations_df
-
-
-def impose_metadata_data_types(df, metadata):
-    """Convert pandas datatypes
-        given mojap-metadata object
-        Original function (impose_metadata_data_types_on_pd_df)
-            from dataengineeringutils.pd_metadata_conformance
-            was failing.
-
-
-    Args:
-        df (pandas dataframe): Dataframe to have dtypes updated
-        metadata (mojap_metadata.Metadata): Table schema
-
-    Returns:
-        pandas dataframe: Original dataframe with new datatypes
-    """
-
-    # Convert schema datatypes to class objects (mapping)
-    typemap = {
-        "string": str,
-        "timestamp": "datetime",
-        "datetime": "datetime",
-        "int": "Int64",
-        "float": float,
-        "bool": bool,
-    }
-
-    # For each column specified in the metadata object
-    for column in metadata["columns"]:
-        # Variable name
-        name = column["name"]
-
-        # Col type from metadata
-        type = column["type"]
-
-        # Mapped column type (e.g. int64 and int32 both mapped to int)
-        map_type = [typemap[key] for key in typemap if re.search(key, type)]
-
-        # Check exactly one match returned
-        if len(map_type) != 1:
-            raise KeyError(f"Data type {type} not found in typemap")
-        else:
-            map_type = map_type[0]
-
-        # If the mapped datatype is a datetime
-        if map_type == "datetime":
-            df[name] = pd.to_datetime(df[name])
-        # Some strings have 'nan' values as the original data was json
-        # The normalized data doesn't always have data and pandas inserts 'nan'
-        elif type == "string":
-            df[name] = df[name].fillna("").astype(map_type)
-        # Else, use the class object
-        else:
-            df[name] = df[name].astype(map_type)
-    return df
-
-
-def impose_exact_conformance_on_pd_df(df, table_metadata):
-    df = impose_metadata_column_order_on_pd_df(
-        df,
-        table_metadata,
-        delete_superflous_colums=True,
-        create_cols_if_not_exist=True,
-    )
-    df = impose_metadata_data_types(df, table_metadata)
-    return df
