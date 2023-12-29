4222efc6669d24791f06a1657f3f3dff7655979c
matt-heery <matt.heery@digital.justice.gov.uk>
2023-12-29 12:09:39 +0000

    refactoring main to reflect changes made in api requests

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,30 +1,24 @@
 from dateutil.parser import parse
-from functions.api_requests import scrape_days_from_api
+from functions.api_requests import retrieve_and_transform_data
 
-from functions.general_helpers import (
-    get_command_line_arguments
-)
+from functions.general_helpers import get_command_line_arguments
 
-from constants import (
-    db_version
-)
+from constants import db_version
 
-#from refresh_app_db import refresh_app_db
+# from refresh_app_db import refresh_app_db
 
 
-
-
-if __name__=="__main__":
-
-
+if __name__ == "__main__":
     # Get command line arguments
     args = get_command_line_arguments()
 
     # Get date from string
     scrape_date = parse(args.scrape_date).strftime("%Y-%m-%d")
-    
-    # Get bookings and locations 
-    # Optionally writes to s3 
-    bookings, locations = scrape_days_from_api(scrape_date, "eod", db_version, args.env, args.skip_write_s3)
 
-    #refresh_app_db()
+    # Get bookings and locations
+    # Optionally writes to s3
+    bookings, locations = retrieve_and_transform_data(
+        db_version, args.env, scrape_date, "eod", args.skip_write_s3
+    )
+
+    # refresh_app_db()
